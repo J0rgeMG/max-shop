e4092ebf8d567bdf534d97ee64c804da9fe60bc9
Jorge <J0rgeMG@live.com>
2025-03-23 22:16:31 -0500

    created log in and sign up views

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
+// Node packages
+const path = require('path');
+
 // Third party packages
 const express = require('express');
 
+// Our packages
+const authRouter = require('./routes/auth.routes');
+
 // Loading app
 const app = express();
+
+// Router configuration
+app.use(authRouter);
+
+// App configuration
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // Starting the app, 3000 as default port
 // <domain>:3000
